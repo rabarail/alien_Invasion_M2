3a43ee257475a583ac5465059098ca3c83774ff1
Rajani <rajani.kami004@gmail.com>
2026-04-19 01:23:10 -0400

    fixed arsenal bullets, added alien fleet

--- arsenal.py
+++ arsenal.py
@@ -17,28 +17,32 @@ class Arsenal:
         self.game = game
         self.settings = game.settings
 
-        self.arsenal = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
 
     def update_arsenal(self):
         """Move all bullets and remove any that have left the screen."""
         
-        self.arsenal.update()
-        for bullet in self.arsenal.copy():
+        self.bullets.update()
+        for bullet in self.bullets.copy():
             if bullet.rect.left > self.game.settings.screen_w: 
-                self.arsenal.remove(bullet)
+                self.bullets.remove(bullet)
         
 
             
     def draw_arsenal(self):
         """Draw all active bullets to the screen."""
-        for bullet in self.arsenal:
+        for bullet in self.bullets:
             bullet.draw_bullet()
 
     def fire_bullet(self):
-        """Create and add a new bullet if under the bullet limit."""
-        if len(self.arsenal) < self.settings.bullet_amount:
+        """Create and add a new bullet if under the bullet limit.
+ 
+        Returns:
+            True if a bullet was fired, False if the limit was reached.
+        """
+        if len(self.bullets) < self.settings.bullet_amount:
             new_bullet = Bullet(self.game)
-            self.arsenal.add(new_bullet)
+            self.bullets.add(new_bullet)
             return True
         return False
         
